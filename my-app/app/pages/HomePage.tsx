22990721c5faa6b96129ba744d57c6f797af114d
Bhashitha Wickramasinghe <chathurabhashitha01@gmail.com>
2024-11-11 10:54:09 +0530

    create project

---
+++ my-app/app/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <View>
+      <Text className='text-3xl text-white font-bold '>HomePage</Text>
+    </View>
+  )
+}
